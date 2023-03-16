a3f175da05596a4cdf1d2cc7cf7015ad10df4579
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-16 12:33:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main(){
+int main(void){
 	
-   puts("\"Programming is like building a multilingual puzzle");
-
-   return(0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0);
 }
 
