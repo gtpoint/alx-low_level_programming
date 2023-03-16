76c7e6a01498699422c0156a9b15f888292e11d3
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-16 15:03:00 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of a int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a float: %d byte(s)", sizeof(float));
 	return (0);
 }
