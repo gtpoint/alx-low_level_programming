365fd6a691082955bdf50fc6104124c9d9943fda
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-16 13:01:33 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/**
- * main - program that print a line with puts function 
+/*
+ * main - program that print a line with puts function
  * return : 0
  */
 
 
-int main(void){
-	
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
