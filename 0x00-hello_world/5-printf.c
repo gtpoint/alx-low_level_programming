886748e0abf68618a4c3b83207057bb6908283ba
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-16 14:15:46 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
