5c52b0c16f01d157296a2c84410c85f688c899f0
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-17 12:39:12 -0700

    print all single digit number from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
