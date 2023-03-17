f4f98bd1d60c824cc7ba831666ef4e1b9cd9e447
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-17 14:53:34 -0700

    print hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		if (i <= 9)
+		{
+			printf("%d", i);
+		}
+		else 
+		{
+			if (i == 10)
+				printf("a");
+			else if (i == 11)
+				printf("b");
+			else if (i == 12)
+				printf("c");
+			else if (i == 13)
+				printf("d");
+			else if (i == 14)
+				printf("e");
+			else if (i == 15)
+				printf("f\n");
+		}
+	}
+	return (0);
+}
