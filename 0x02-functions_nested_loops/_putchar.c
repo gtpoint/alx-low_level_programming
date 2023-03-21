86d22dffc4ba49414f7a0161a7300e39842b705d
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-21 11:49:25 -0700

    pushed _putchar function

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - this is the print function 
+ *            print one caracter
+ * @caract: the character to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned
+ */
+int _putchar(char caract)
+{
+	return (Write(1, &c, 1));
+}
