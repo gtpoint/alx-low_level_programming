bf9801df72f5235eb39d9a896a2247b4f2931f76
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-21 15:42:53 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - this is function that prints the sign of a number
+ *
+ * @n: the number chekced 
+ *
+ * Return: 1 if n is greater than zero ,
+ *         0 if n is zero ,
+ *         -1 is n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
