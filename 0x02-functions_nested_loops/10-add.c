e0ca1705b968928017aa926786cf2e59fb17ba1b
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-22 07:56:45 -0700

    created add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - this is a function that adds two integers and returns the result
+ * @a: the number one
+ * @b: the number two
+ *
+ * Return: the result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
