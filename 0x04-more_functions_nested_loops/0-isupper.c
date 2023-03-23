b7e84c4b1cf85803d7f9826d66492deff9bb6c60
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-23 14:56:15 -0700

    created file 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isupper - this function that checks for uppercase character.
+ * 
+ * @c: the check character
+ * Return: 1 if character is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
