804d51bb9b6e355c643a2f4b613b7fddb44e2d94
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-23 15:44:39 -0700

    created file 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
