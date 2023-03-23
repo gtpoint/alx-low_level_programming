68429f2521d112a07e0cc413744bdf256f273fef
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-23 15:01:28 -0700

    craeted file 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * _isupper - this function that checks for uppercase character.
- * 
+ *
  * @c: the check character
  * Return: 1 if character is uppercase, 0 otherwise
  */
