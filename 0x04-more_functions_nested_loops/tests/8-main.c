33a6491c660e6671d10eb4ccef1dd0bd7534916a
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-23 17:18:50 -0700

    created test file 8

---
+++ 0x04-more_functions_nested_loops/tests/8-main.c
@@ -0,0 +1,14 @@
+#include "../main.h"
+
+/**
+ * main - this is the main function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
