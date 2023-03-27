861c3af12d950ade2e4314eab6d9b0c4b6ee806f
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-27 08:09:30 -0700

    updated main header

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+int _strlen(const char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
