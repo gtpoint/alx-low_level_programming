d60d001dbafb612e05c3b3c324ec86ef86c0f095
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-27 07:41:22 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function taht takes a pointer to
+ * an int as a parameter and updates the value it points to 98.
+ * @n: parameter
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
