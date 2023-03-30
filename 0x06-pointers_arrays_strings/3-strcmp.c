6ddcf72b596ba6d655ca4d18dede73499c5c55d3
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-03-29 20:13:49 -0700

    created file 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - this function that compares two strings.
+ * @s1: the first parameter
+ * @s2: the second parameter
+ * Return: the result of comparaison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
