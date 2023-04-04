fe79a47502a83a18f60ba1a98588389813438638
Oussama El aouzari <125877426+BigGtpoint@users.noreply.github.com>
2023-04-04 18:44:18 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		s[index] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 
 	return (s);
 }
