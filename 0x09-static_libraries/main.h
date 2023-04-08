d1811fdd128906c34501f0742cb85fe2c9444c6e
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-04-07 17:08:24 -0700

    updated file main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -9,7 +9,7 @@ int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
-int _strlen(char *s);
+int _strlen(const char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
