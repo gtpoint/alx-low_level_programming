f34e7564450d0a012d3be899710ac5346cd5a978
BigGtpoint <elaouzariofficiel12@gmail.com>
2023-04-08 16:32:47 -0700

    created file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: count argument.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
