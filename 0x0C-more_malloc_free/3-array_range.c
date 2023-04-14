4180e89dcc3ca7a5991c820fefc7fd570ce0d6cf
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-04-14 20:50:28 +0200

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -23,8 +23,8 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = min; i <= max - min + 1; i++)
-		arr[i] = i;
+	for (i = 0; i < max - min + 1; i++)
+		arr[i] = min++;
 
 	return (arr);
 }
