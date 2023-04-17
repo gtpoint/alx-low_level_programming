234b6df1249ebd03ed281049934330675bac4a80
gtpoint <elaouzariofficiel12@gmail.com>
2023-04-17 10:39:15 -0700

    cerated file 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this function that prints the name of the file it was compiled from, followed by a new line.
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
