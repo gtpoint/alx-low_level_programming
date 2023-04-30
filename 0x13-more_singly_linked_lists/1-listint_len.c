cd88d0714a7a1dff35c226e0e76fdc025308f3b5
gtpoint <elaouzariofficiel12@gmail.com>
2023-04-30 15:10:27 -0700

    created file 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - this function that Returns the number of elements
+ *               in a linked list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the list @h.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while(h)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
