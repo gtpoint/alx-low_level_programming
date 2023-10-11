7cf5d328dac871bcd6f1828ffcc0df8673d7ffc1
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-10-11 13:11:46 +0200

    Create 1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers.
+ * @array: pointer to the first element of the array.
+ * @size: number of elements in the array.
+ * @value: the value to search for.
+ * Return: the index where value is located or -1 if not found.
+ */
+int binary_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1;
+    }
+
+    size_t left = 0;
+    size_t right = size - 1;
+
+    while (left <= right) {
+        size_t mid = left + (right - left) / 2;
+
+        printf("Searching in array: ");
+        for (size_t i = left; i <= right; i++) {
+            if (i == right) {
+                printf("%d", array[i]);
+            } else {
+                printf("%d, ", array[i]);
+            }
+        }
+        printf("\n");
+
+        if (array[mid] == value) {
+            return mid;
+        } else if (array[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
