6d63b47752e5b25194a877b78a815a92959be243
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-10-11 16:51:44 +0200

    Create 100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers using Jump search.
+ * @array: pointer to the first element of the array.
+ * @size: number of elements in the array.
+ * @value: the value to search for.
+ * Return: the index where value is located or -1 if not found.
+ */
+
+int jump_search(int *array, size_t size, int value){
+  size_t step = (size_t)sqrt(size);
+  size_t start = 0;
+  size_t end = step;
+
+  if (array == NULL) {
+    return (-1);
+  }
+
+  while (end < size && array[end] < value) {
+    printf("Value checked array[%lu] = [%d]\n", end, array[end]);
+    start = end;
+    end += step;
+  }
+
+  printf("Value found between indexes [%lu] and [%lu]\n", start, end);
+
+  while (start <= end && start < size) {
+    printf("Value checked array[%lu] = [%d]\n", start, array[start]);
+    if (array[start] == value) {
+      return (start);
+    }
+    start++;
+  }
+  
+  return (-1);
+}
